9888642b8b2e06d9502658b02142967fdb2b505d
Fiton Frangu <fitonn@msn.com>
2020-12-13 12:40:14 +0100

    Add project files.

---
+++ TreeHeight/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TreeHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 7, 10, 12, 6, 8 };
+            Node root = null;
+
+            foreach (var item in arr)
+            {
+                root = Tree.Insert(root, item);
+            }
+
+            Console.WriteLine("Tree height is : " +
+                                        Tree.CalculateDepth(root));
+        }
+    }
+}
---
+++ TreeHeight/Tree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeHeight
+{
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    public static class Tree
+    {
+        public static Node root;
+
+        public static int CalculateDepth(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                int leftDepth = CalculateDepth(node.left);
+                int rightDepth = CalculateDepth(node.right);
+
+                if (leftDepth > rightDepth)
+                    return (leftDepth + 1);
+                else
+                    return (rightDepth + 1);
+            }
+        }
+
+        public static Node Insert(Node root, int data)
+        {
+            if (root == null) 
+                root = new Node(data);
+            else if (data <= root.data) 
+                root.left = Insert(root.left, data);
+            else if (data > root.data) 
+                root.right = Insert(root.right, data);
+
+            return root;
+        }
+    }
+}
